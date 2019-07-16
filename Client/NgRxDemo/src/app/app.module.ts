cb619dcf6c99ff3f3c97ae5ffc6c411358604839
Erik Parso <eparso@sigp.info>
2019-07-16 11:55:09 +0200

    Style toolbar and nav bar.

--- Client/NgRxDemo/src/app/app.module.ts
+++ Client/NgRxDemo/src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers } from './app.reducer';
 import { EffectsModule } from '@ngrx/effects';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
    declarations: [
@@ -29,7 +30,8 @@ import {MatButtonModule} from '@angular/material/button';
       MatMenuModule,
       MatIconModule,
       MatToolbarModule,
-      MatButtonModule
+      MatButtonModule,
+      MatSidenavModule
    ],
    providers: [],
    bootstrap: [
